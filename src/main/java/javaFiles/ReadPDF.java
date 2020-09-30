46228fab1fdcb99efc35118892bb7249ece3309c
jamshed7 <43277097+jamshed7@users.noreply.github.com>
2020-09-29 21:34:54 -0500

    Initial commit

---
+++ src/main/java/javaFiles/ReadPDF.java
@@ -0,0 +1,13 @@
+package javaFiles;
+
+public class ReadPDF {
+	public ReadPDF(){}
+	
+	public static String read(byte[] bArray) {
+		
+		String result = new String(bArray);
+		
+		return result;
+	}
+
+}
